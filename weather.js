463bd792b2f62883c60c2bb64188345552520de1
juliebogart <j-bogart@kellogg.northwestern.edu>
2016-01-31 17:36:10 -0600

    added css styles sheet

--- weather.js
+++ weather.js
@@ -5,10 +5,10 @@ $(function() {
     // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
 
-    var markup = "<p>The weather is currently " + data.currently.summary + " and " + data.currently.temperature + " Degrees</p><p>The three day forecast is: </p>" +
-     "<li> Tomorrow: " + data.daily.data[1].summary + "</li>" +
-     "<li> In Two Days: " + data.daily.data[2].summary + "</li>" +
-     "<li> In Three Days: " + data.daily.data[3].summary + "</li>"
+    var markup = "<p><b>The weather is currently: </b> " + data.currently.summary + " and " + data.currently.temperature + " Degrees</p><p><b>The three day forecast is: </b> </p>" +
+     "<li> <b>Tomorrow: </b>" + data.daily.data[1].summary + "</li>" +
+     "<li> <b>In Two Days: </b>" + data.daily.data[2].summary + "</li>" +
+     "<li> <b>In Three Days: </b>" + data.daily.data[3].summary + "</li>"
 
 //data.dailydata[1].summary
  //data.currently.windspeed
