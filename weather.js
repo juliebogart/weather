78c386b447f74db3b9d8e530051a4d6a5b74d422
juliebogart <j-bogart@kellogg.northwestern.edu>
2016-01-31 17:26:07 -0600

    added all required data

--- weather.js
+++ weather.js
@@ -2,12 +2,16 @@ $(function() {
   var handleWeatherResponse = function(data) {
     console.log(data);
 
-    // Put your code here to change the "markup" variable. 
+    // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
-    
-    var markup = "The weather report... " + "will be here when I finish my homework.";
-    
-    
+
+    var markup = "<p>The weather is currently " + data.currently.summary + " and " + data.currently.temperature + " Degrees</p><p>The three day forecast is: </p>" +
+     "<li> Tomorrow: " + data.daily.data[1].summary + "</li>" +
+     "<li> In Two Days: " + data.daily.data[2].summary + "</li>" +
+     "<li> In Three Days: " + data.daily.data[3].summary + "</li>"
+
+//data.dailydata[1].summary
+ //data.currently.windspeed
     // End of your code
 
     $('.weather-report').html(markup);
